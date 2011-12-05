d15e728e704644258c8820989af79b06744d958d
Sagar Virani <sagarvirani@gmail.com>
2011-12-05 15:33:55 +0530

    Merged

---
+++ src/org/dynasoar/monitor/NodeMonitor.java
@@ -0,0 +1,68 @@
+package org.dynasoar.monitor;
+
+import org.apache.log4j.Logger;
+import org.dynasoar.communicator.NodeCommunicator;
+import org.dynasoar.service.ServiceMonitor;
+
+/**
+ * NodeMonitor is responsible for monitoring current node. Along with monitoring
+ * Node's load and services, it will also make sure all the supporting threads
+ * are running.
+ * 
+ * @author Rakshit Menpara
+ */
+public class NodeMonitor implements Runnable {
+	private static NodeMonitor current = null;
+	private static Logger logger = Logger.getLogger(NodeMonitor.class);
+	private static Thread th = null;
+
+	private boolean shutdown = false;
+	private int interval = 1000;
+
+	public static void start() {
+		current = new NodeMonitor();
+		Thread th = new Thread(current, "NodeMonitor");
+		th.start();
+	}
+
+	public static NodeMonitor get() {
+		if (current == null) {
+			start();
+		}
+		return current;
+	}
+
+	public static boolean isRunning() {
+		if (current == null) {
+			return false;
+		}
+
+		return th.isAlive();
+	}
+
+	@Override
+	public void run() {
+		while (!shutdown) {
+			if (!NodeCommunicator.isRunning()) {
+				// Invoke TCP communication thread
+				NodeCommunicator.start();
+			}
+
+			if (!ServiceMonitor.isRunning()) {
+				// Invoke the service repository monitor
+				ServiceMonitor.start();
+			}
+
+			try {
+				Thread.sleep(interval);
+			} catch (InterruptedException e) {
+				logger.error("Thread busy.", e);
+			}
+		}
+	}
+
+	public void shutdown() {
+		logger.info("Shutting Down NodeMonitor");
+		shutdown = true;
+	}
+}
