2cc08e379cf3baee68d4da68457dd069eefc19ae
Rakshit Menpara <deltasquare4@gmail.com>
2012-01-05 21:47:25 +0530

    Allowing the jar to load config as a parameter

--- src/org/dynasoar/Bootstrap.java
+++ src/org/dynasoar/Bootstrap.java
@@ -5,7 +5,6 @@ import org.dynasoar.monitor.NodeMonitor;
 import org.dynasoar.service.ServiceMonitor;
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
-    
 
 /**
  * Initializes and starts up the application.
@@ -14,23 +13,30 @@ import org.apache.log4j.BasicConfigurator;
  */
 
 public class Bootstrap {
-	
+
 	private static Logger log = Logger.getLogger(Bootstrap.class);
-	
+
 	public static void main(String args[]) {
 
-		 // Set up a simple configuration that logs on the console.
-     		 BasicConfigurator.configure();
-		
-		// Read configuration file
-		Configuration.readConfiguration();
+		// Set up a simple configuration that logs on the console.
+		BasicConfigurator.configure();
+
+		// Check if configuration path is specified as a parameter
+		if (args.length > 2) {
+			String path = args[2];
+
+			// Read configuration file
+			Configuration.readConfiguration(path);
+		} else {
+			// Read configuration file
+			Configuration.readConfiguration();
+		}
 
 		// Start up ServiceMonitor
 		ServiceMonitor.start();
-		
+
 		// Start up NodeMonitor
 		NodeMonitor.start();
-		
-		
+
 	}
 }
